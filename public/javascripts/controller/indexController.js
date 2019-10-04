d1ee734ff2cccaa067f91130b9b9cb5de30268d3
Yusuf ŞENEL <ysf.senel@gmail.com>
2019-10-04 23:09:56 +0300

    newUser

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,12 +1,25 @@
 app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=>{
+    $scope.init=()=>{
+    const username=prompt('please enter a username');
+    if(username)
+        initSocket(username);
+    else
+        return false;
+    };
+    function initSocket (username){
+        const connectionOptions={
+            reconnectionAttempts :3,
+            reconnectionDelay:600
+        }
+        indexFactory.connectSocket('http://localhost:3000',connectionOptions)
+            .then((socket)=>{
+                socket.emit('newUser',{username});
+           // console.log('bağlantı gerçekleşti',socket);
+            }).catch((err)=>{
+            console.log(err);
+             });
+    };
 
-indexFactory.connectSocket('http://localhost:3000',{
-    reconnectionAttempts :3,
-    reconnectionDelay:600
-}).then((socket)=>{
-    console.log('bağlantı gerçekleşti',socket);
-    }).catch((err)=>{
-    console.log(err);
-});
+
 
 }]);
